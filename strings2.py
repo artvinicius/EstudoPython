c211cfab8fd5bce38bf5813235aac2ec896b18d7
Arthur Vinícius <arthurviny1999@gmail.com>
2020-03-16 13:18:28 -0300

    Adicione Strings parte 2 com metodos

---
+++ strings2.py
@@ -0,0 +1,14 @@
+a = "Arthur"
+b = "Vinicius"
+
+concatenar = a + " " + b + "\n"
+
+#lower deixar caracteris minusculo
+# upper deixa maisculo  
+print(concatenar)
+print(concatenar.lower())
+print(concatenar.upper())
+
+concatener = concatenar.upper()
+print (concatenar)
+
---
+++ strings2_2.py
@@ -0,0 +1,16 @@
+
+minha_string = "Arthur Vincius Gomes "
+#minha_lista = minha_string.split("r")
+#print(minha_lista)
+
+#case sensitive
+
+#busca por palavra 
+
+# busca = minha_string.find("Gomes")
+# print(busca)
+# print (minha_string[busca:])
+
+#Trocando o nome de uma plavra especifica = replace
+minha_string = minha_string.replace("Arthur", "Thuthut")
+print(minha_string)
